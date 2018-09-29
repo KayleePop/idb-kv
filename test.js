b15b48cad01269e279425edf7cdd51d4d32a58ce
KayleePop <34007889+KayleePop@users.noreply.github.com>
2018-09-29 15:25:57 -0500

    Switch to muggle for tests

--- test.js
+++ test.js
@@ -1,38 +1,36 @@
-const test = require('tape-promise/tape')
+const test = require('muggle-test')
+const assert = require('muggle-assert')
 const Idbkv = require('./index.js')
+const sleep = (delay) => new Promise(resolve => setTimeout(resolve, delay))
 
 // make sure store is empty
-async function setupStore (storeName, opts) {
+async function createCleanStore (storeName, opts) {
   let store = new Idbkv(storeName)
   await store.destroy()
   return new Idbkv(storeName, opts)
 }
 
-function sleep (delay) {
-  return new Promise((resolve, reject) => setTimeout(resolve, delay))
-}
-
-test('set then get', async (t) => {
-  let store = await setupStore('setTest')
+test('set then get', async () => {
+  let store = await createCleanStore('setTest')
 
   await store.set('key', 'value')
   let value = await store.get('key')
-  t.equals(value, 'value', 'seperate transactions')
+  assert.equal(value, 'value', 'seperate transactions')
 
   store.set('key2', 'value2')
   let value2 = await store.get('key2')
-  t.equals(value2, 'value2', 'same transaction')
+  assert.equal(value2, 'value2', 'same transaction')
 })
 
-test('get nonexistent key', async (t) => {
-  let store = await setupStore('undefinedGetTest')
+test('get nonexistent key', async () => {
+  let store = await createCleanStore('undefinedGetTest')
 
   let value = await store.get('key')
-  t.equals(value, undefined, 'should be undefined')
+  assert.equal(value, undefined, 'should be undefined')
 })
 
-test('data should persist to a new instance', async (t) => {
-  let store = await setupStore('persistTest')
+test('data should persist to a new instance', async () => {
+  let store = await createCleanStore('persistTest')
 
   await store.set('key', 'value')
   await store.close()
@@ -42,61 +40,58 @@ test('data should persist to a new instance', async (t) => {
   let store2 = new Idbkv('persistTest')
 
   let value = await store2.get('key')
-  t.equals(value, 'value')
+  assert.equal(value, 'value')
 })
 
-test('overwrite a key', async (t) => {
-  let store = await setupStore('overwriteTest')
+test('overwrite a key', async () => {
+  let store = await createCleanStore('overwriteTest')
 
   await store.set('key', 'value')
   await store.set('key', 'overwrite')
   let value = await store.get('key')
-  t.equals(value, 'overwrite', 'different transactions')
+  assert.equal(value, 'overwrite', 'different transactions')
 
   store.set('key2', 'value')
   store.set('key2', 'overwrite')
   let value2 = await store.get('key2')
-  t.equals(value2, 'overwrite', 'same transaction')
+  assert.equal(value2, 'overwrite', 'same transaction')
 })
 
-test('order is preserved in batch', async (t) => {
-  let store = await setupStore('orderTest')
-
-  t.plan(3)
+test('order is preserved in batch', async () => {
+  let store = await createCleanStore('orderTest')
 
   store.set('key', 'value')
-  store.get('key')
-    .then((value) => t.equals(value, 'value'))
-    .catch(err => t.error(err))
+  const setValue = store.get('key')
   store.set('key', 'overwrite')
-  store.get('key')
-    .then((value) => t.equals(value, 'overwrite'))
-    .catch(err => t.error(err))
+  const overwriteValue = store.get('key')
   store.delete('key')
-  let value = await store.get('key')
-  t.equals(value, undefined)
+  const deleteValue = store.get('key')
+
+  assert.equal(await setValue, 'value')
+  assert.equal(await overwriteValue, 'overwrite')
+  assert.equal(await deleteValue, undefined)
 })
 
-test('store an array', async (t) => {
-  let store = await setupStore('functionTest')
+test('store an array', async () => {
+  let store = await createCleanStore('functionTest')
 
   store.set('array', [1, 1, 1, 1, 1])
   let array = await store.get('array')
   let sum = array.reduce((sum, curr) => sum + curr)
-  t.equals(sum, 5, 'array should be preserved through set and get')
+  assert.equal(sum, 5, 'array should be preserved through set and get')
 })
 
-test('delete a key', async (t) => {
-  let store = await setupStore('deleteTest')
+test('delete a key', async () => {
+  let store = await createCleanStore('deleteTest')
 
   store.set('key', 'value')
   store.delete('key')
   let value = await store.get('key')
-  t.equals(value, undefined)
+  assert.equal(value, undefined)
 })
 
-test('destroy a store', async (t) => {
-  let store = await setupStore('destroyTest')
+test('destroy a store', async () => {
+  let store = await createCleanStore('destroyTest')
 
   store.set('key1', 'value1')
   store.set('key2', 'value1')
@@ -104,78 +99,51 @@ test('destroy a store', async (t) => {
   await store.destroy()
   store = new Idbkv('destroyTest')
 
-  t.equals(await store.get('key1'), undefined)
-  t.equals(await store.get('key2'), undefined)
-  t.equals(await store.get('key3'), undefined)
+  assert.equal(await store.get('key1'), undefined)
+  assert.equal(await store.get('key2'), undefined)
+  assert.equal(await store.get('key3'), undefined)
 })
 
-test('seperate stores should not interact', async (t) => {
-  let store1 = await setupStore('storeInteractionTest1')
-  let store2 = await setupStore('storeInteractionTest2')
+test('seperate stores should not interact', async () => {
+  let store1 = await createCleanStore('storeInteractionTest1')
+  let store2 = await createCleanStore('storeInteractionTest2')
 
   store1.set('key', 'value1')
   store2.set('key', 'value2')
 
   let value1 = await store1.get('key')
-  t.equals(value1, 'value1', 'putting to the same key of a different store should not affect this store')
+  assert.equal(value1, 'value1', 'putting to the same key of a different store should not affect this store')
 
   await store1.delete('key')
 
   let value2 = await store2.get('key')
-  t.equals(value2, 'value2', 'deleting the same key in a different store should not affect this store')
+  assert.equal(value2, 'value2', 'deleting the same key in a different store should not affect this store')
 })
 
-test('closed instance rejects new actions', async (t) => {
-  let store = await setupStore('closedErrorTest')
-  t.plan(6)
+test('closed instance rejects new actions', async () => {
+  let store = await createCleanStore('closedErrorTest')
 
   store.close()
-  // try catch await style
-  try {
-    await store.get('key')
-    t.fail('get() on a closed instance should throw error')
-  } catch (err) {
-    t.equals(err.message, 'This Idbkv instance is closed')
-  }
-  try {
-    await store.set('key', 'value')
-    t.fail('set() on a closed instance should throw error')
-  } catch (err) {
-    t.equals(err.message, 'This Idbkv instance is closed')
-  }
-  try {
-    await store.delete('key')
-    t.fail('delete() on a closed instance should throw error')
-  } catch (err) {
-    t.equals(err.message, 'This Idbkv instance is closed')
-  }
-  // .catch style
-  store.get('key')
-    .catch(err => t.equals(err.message, 'This Idbkv instance is closed'))
-  store.set('key', 'value')
-    .catch(err => t.equals(err.message, 'This Idbkv instance is closed'))
-  store.delete('key')
-    .catch(err => t.equals(err.message, 'This Idbkv instance is closed'))
+
+  const closedError = new Error('This Idbkv instance is closed')
+  await assert.rejects(store.get('key'), closedError, 'get should reject')
+  await assert.rejects(store.set('key', 'value'), closedError, 'set should reject')
+  await assert.rejects(store.delete('key'), closedError, 'delete should reject')
 })
 
-test('close() waits for queued actions', async (t) => {
-  let store = await setupStore('closedDrainTest')
+test('close() waits for queued actions', async () => {
+  let store = await createCleanStore('closedDrainTest')
 
-  store.set('key', 'first')
-  store.delete('key')
   store.set('key', 'value')
   await store.close()
   store = new Idbkv('closedDrainTest')
 
   let value = await store.get('key')
-  t.equals(value, 'value')
+  assert.equal(value, 'value')
 })
 
-test('indexedDB failing to open rejects actions', (t) => {
-  t.plan(7)
-
-  // change to error after 100ms
-  // it's the last test, so overwriting this shouldn't matter
+// change indexedDB.open to always fail after 100ms
+function overwriteIdbOpen () {
   window.indexedDB.open = () => {
     const request = {}
     setTimeout(() => {
@@ -184,43 +152,42 @@ test('indexedDB failing to open rejects actions', (t) => {
     }, 100)
     return request
   }
-  const store = new Idbkv('errorFailActionTest')
+}
 
-  // queued actions before the failure
-  store.get('key')
-    .then(() => t.fail('queued get should reject after db fails to open'))
-    .catch(err => {
-      t.equals(err.message, 'error opening the indexedDB database named errorFailActionTest: Error: idb error')
-    })
-  store.set('key', 'value')
-    .then(() => t.fail('queued set should reject after db fails to open'))
-    .catch(err => {
-      t.equals(err.message, 'error opening the indexedDB database named errorFailActionTest: Error: idb error')
-    })
-  store.delete('key')
-    .then(() => t.fail('queued delete should reject after db fails to open'))
-    .catch(err => {
-      t.equals(err.message, 'error opening the indexedDB database named errorFailActionTest: Error: idb error')
-    })
+test('indexedDB failing to open rejects queued actions', async () => {
+  const originalOpen = window.indexedDB.open
+  overwriteIdbOpen()
 
-  store.db.catch((err) => {
-    t.equals(err.message, 'error opening the indexedDB database named errorFailActionTest: Error: idb error')
+  const store = new Idbkv('dbFailRejectsQueued')
 
-    // new actions after the failure
-    store.get('key')
-      .then(() => t.fail('get after idb failing to open should reject'))
-      .catch(err => {
-        t.equals(err.message, 'error opening the indexedDB database named errorFailActionTest: Error: idb error')
-      })
-    store.set('key', 'value')
-      .then(() => t.fail('set after idb failing to open should reject'))
-      .catch(err => {
-        t.equals(err.message, 'error opening the indexedDB database named errorFailActionTest: Error: idb error')
-      })
-    store.delete('key')
-      .then(() => t.fail('delete after idb failing to open should reject'))
-      .catch(err => {
-        t.equals(err.message, 'error opening the indexedDB database named errorFailActionTest: Error: idb error')
-      })
-  })
+  const expectedError =
+    new Error('error opening the indexedDB database named dbFailRejectsQueued: Error: idb error')
+
+  // queue all before db fails to open
+  await Promise.all([
+    assert.rejects(store.get('key'), expectedError, 'get'),
+    assert.rejects(store.set('key', 'value'), expectedError, 'set'),
+    assert.rejects(store.delete('key'), expectedError, 'delete')
+  ])
+
+  window.indexedDB.open = originalOpen
+})
+
+test('indexedDB failing to open rejects new actions', async () => {
+  const originalOpen = window.indexedDB.open
+  overwriteIdbOpen()
+
+  const store = new Idbkv('dbFailRejectsNew')
+
+  const expectedError =
+    new Error('error opening the indexedDB database named dbFailRejectsNew: Error: idb error')
+
+  // wait until db fails
+  await assert.rejects(store.db, 'db should fail to open')
+
+  await assert.rejects(store.get('key'), expectedError, 'get')
+  await assert.rejects(store.set('key', 'value'), expectedError, 'set')
+  await assert.rejects(store.delete('key'), expectedError, 'delete')
+
+  window.indexedDB.open = originalOpen
 })
